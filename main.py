d89fc4c80bb3584e05018182ddfbc18ca541bfee
luis2 <luis212urbina@gmail.com>
2019-02-28 13:52:30 -0600

    basic behaviours added

--- main.py
+++ main.py
@@ -2,45 +2,51 @@
 import simpy
 import random
 
-# el proceso process muestra un veh�culo que se estaciona un tiempo
-# y luego se conduce otro lapso de tiempo
-def Process(nombre,env,process_time,ram):
-    global totalDia  # :( mala practica, pero ni modo
 
-    # Simular que esta conduciendo un tiempo antes de llegar a la gasolinera
-    yield env.timeout(process_time)
-    
-    # llegando a la gasolinera
-    horaLlegada = env.now
 
-    # simular que necesita un tiempo para processgar gasolina. Probablemente
-    # si es processro peque�o necesita menos tiempo y si es proceso grande mas tiempo
-    tiempoProceso = random.randint(1, 7)
-    print ('%s llega a las %f necesita %d para hechar gasolina' % (nombre,horaLlegada,tiempoProceso))
-    
-    # ahora se dirige a la bomba de gasolina,
-    # pero si hay otros carros, debe hacer cola
-    with ram.request() as turno:
-        yield turno      #ya puso la manguera de gasolina en el proceso!
-        yield env.timeout(tiempoProceso) #hecha gasolina por un tiempo
-        print ('%s sale de gasolinera a las %f' % (nombre, env.now))
-        #aqui el proceso hace un release automatico de la bomba de gasolina
-        
-    tiempoTotal = env.now - horaLlegada
-    print ('%s se tardo %f' % (nombre, tiempoTotal))
-    totalDia = totalDia + tiempoTotal
-           
-
-# ----------------------
-
-env = simpy.Environment() #ambiente de simulaci�n
-ram = simpy.Resource(env,capacity = 1)
+env = simpy.Environment()
+ram = simpy.Container(env, init=100, capacity=100)
+cpu = simpy.Resource(env, capacity=1)
 random.seed(10) # fijar el inicio de random
 
-totalDia = 0
-for i in range(5):
-    env.process(Process('proceso %d'%i,env,random.expovariate(1.0/10),ram))
+def Process(name, env , instruction_qty , needed_ram, ram, cpu):
+    state = 0
+    print(ram.level)
+    while True:
+        if (state == 0):
+            print(state) #todo
+            print('Process %s requesting %s of RAM at %s' % (name, needed_ram ,env.now))
+            if (ram.level > needed_ram):
+                yield ram.get(needed_ram)
+                yield env.timeout(1)
+                state = 1
+            else:
+                print("insuficient RAM in the system")
+        elif(state != 0 ):
+            print(state) #
+            #Changed State, initialize ready process
+            print('Process %s requesting CPU access at %s' % (name, env.now))
+            with cpu.request() as cpu_request:
+                yield cpu_request
+                print('Process %s got CPU access at %s' % (name, env.now))
+                state = 2
+                if (instruction_qty <= 3):
+                    instruction_qty = 0
+                    yield env.timeout(1)
+                    cpu.release(cpu_request)
+                    state = 4 #terminated
+                    print("Process ended")
+                else:
+                    instruction_qty -= 3
+                    cpu.release(cpu_request)
+                    #In case the process needs to wait for I/O operations
+                    random_integer = random.randint(1,2)
+                    if (random_integer == 1):
+                        waiting_timeout = 1 #can be any number
+                        state = 3
+                        yield env.timeout(waiting_timeout) # After this time, the process will be passed to ready again
+                           
+myProcess = env.process(Process("A",env,10,10,ram,cpu))
 
-env.run(until=50)  #correr la simulaci�n hasta el tiempo = 50
-
-print ("tiempo promedio por veh�culo es: ", totalDia/5.0)
+env.run(until=5)
+#With a for loop, not with until
